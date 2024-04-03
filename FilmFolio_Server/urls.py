9ad77ca5e1fdd72200a25c5387573afd8baccf04
Md. Huzaifa <saadhuzaifa2497@gmail.com>
2024-04-03 19:31:10 +0600

    static files added

--- FilmFolio_Server/urls.py
+++ FilmFolio_Server/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path
 from api.views import app
-
+from django.conf.urls.static import static
+from django.conf import settings
 urlpatterns = [path("admin/", admin.site.urls), path("", app.urls)]
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
