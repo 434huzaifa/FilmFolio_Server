a0a81afb14f9ba0b88feebabee9d5fa01afc1265
Md. Huzaifa <saadhuzaifa2497@gmail.com>
2024-04-03 14:23:45 +0600

    initial

---
+++ api/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+# Create your models here.
+
+class User (models.Model):
+    name=models.CharField(max_length=100,null=True,default=None)
+    phone=models.CharField(max_length=50,null=True,default=None)
+    password=models.CharField(max_length=20,null=True,default=None)
+    email=models.EmailField(max_length=254,null=True,default=None)
+    
+class Movie(models.Model):
+    name=models.CharField(max_length=100,null=True,default=None)
+    genre=models.CharField(max_length=20,null=True,default=None)
+    rating=models.CharField(max_length=10,null=True,default=None)
+    release_date=models.DateField(null=True,default=None)
+
+class Rating(models.Model):
+    user_id=models.ForeignKey(User,on_delete=models.CASCADE)
+    movie_id=models.ForeignKey(Movie,on_delete=models.CASCADE)
+    rating=models.FloatField(null=True, default=None)
